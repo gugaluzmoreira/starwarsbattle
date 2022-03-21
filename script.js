0c16f443d5097b5eaa55569c36b9f97820b7a490
gugaluzmoreira <102001765+gugaluzmoreira@users.noreply.github.com>
2022-03-20 21:39:32 -0300

    Add files via upload
    Adicionei arquivos html, CSS e JS

---
+++ script.js
@@ -0,0 +1,726 @@
+var carta0 = {
+  nome: "",
+  avatar: "https://madisonkennedyblog.files.wordpress.com/2014/08/img_0353.png",
+  atributos: {
+    ataque: "",
+    defesa: "",
+    chakra: ""
+  }
+};
+
+var carta1 = {
+  nome: "Darth Vader",
+  avatar:
+    "https://disneyplusbrasil.com.br/wp-content/uploads/2021/06/Darth-Vader-serie-Disney-Plus-1024x576.jpg",
+  atributos: {
+    força: 9,
+    velocidade: 7,
+    habilidade: 9,
+    inteligencia: 8
+  }
+};
+
+var carta2 = {
+  nome: "Obi Wan Kenobi",
+  avatar: "https://hqrock.files.wordpress.com/2019/08/obi-wan-lightsaber.jpg",
+  atributos: {
+    força: 9,
+    velocidade: 8,
+    habilidade: 9,
+    inteligencia: 9
+  }
+};
+
+var carta3 = {
+  nome: "Luke Skywalker",
+  avatar:
+    "https://rollingstone.uol.com.br/media/_versions/mark_hamill_in_star_wars-_episodio_vi_-_o_retorno_do_jedi__reproducao_widelg.jpg",
+  atributos: {
+    força: 10,
+    velocidade: 8,
+    habilidade: 10,
+    inteligencia: 9
+  }
+};
+
+var carta4 = {
+  nome: "Qui Gon Jinn",
+  avatar:
+    "https://static.wikia.nocookie.net/swfanon/images/d/d6/QuiGonPortrait-SWE.jpg",
+  atributos: {
+    força: 8,
+    velocidade: 7,
+    habilidade: 8,
+    inteligencia: 8
+  }
+};
+
+var carta5 = {
+  nome: "Darth Maul",
+  avatar: "https://cdn.mos.cms.futurecdn.net/qQnhpYGsovwgZiiCUm5TsA.jpg",
+  atributos: {
+    força: 7,
+    velocidade: 9,
+    habilidade: 8,
+    inteligencia: 6
+  }
+};
+
+var carta6 = {
+  nome: "Kylo Ren",
+  avatar: "https://upload.wikimedia.org/wikipedia/pt/f/f7/Kylo_Ren.jpg",
+  atributos: {
+    força: 9,
+    velocidade: 8,
+    habilidade: 8,
+    inteligencia: 8
+  }
+};
+
+var carta7 = {
+  nome: "General Grievous",
+  avatar:
+    "https://static.wikia.nocookie.net/disney/images/6/65/Profile_-_General_Grievous.png",
+  atributos: {
+    força: 6,
+    velocidade: 7,
+    habilidade: 7,
+    inteligencia: 6
+  }
+};
+
+var carta8 = {
+  nome: "Master Yoda",
+  avatar: "https://upload.wikimedia.org/wikipedia/pt/4/45/Yoda.jpg",
+  atributos: {
+    força: 10,
+    velocidade: 7,
+    habilidade: 10,
+    inteligencia: 9
+  }
+};
+var carta9 = {
+  nome: "Mace Windu",
+  avatar:
+    "https://www.giantbomb.com/a/uploads/square_medium/0/4387/561803-136041mace_20windu.jpg",
+  atributos: {
+    força: 9,
+    velocidade: 7,
+    habilidade: 8,
+    inteligencia: 8
+  }
+};
+var carta10 = {
+  nome: "Rey",
+  avatar:
+    "https://i.pinimg.com/originals/ef/ec/fc/efecfc9c405eb26e83912b892ca4b3f3.jpg",
+  atributos: {
+    força: 10,
+    velocidade: 8,
+    habilidade: 9,
+    inteligencia: 8
+  }
+};
+var carta11 = {
+  nome: "Imperador Palpatine",
+  avatar: "https://rollingstone.uol.com.br/media/uploads/9_sheev_palpatine.jpg",
+  atributos: {
+    força: 10,
+    velocidade: 6,
+    habilidade: 10,
+    inteligencia: 10
+  }
+};
+var carta12 = {
+  nome: "Ahsoka Tano",
+  avatar:
+    "https://poltronanerd.com.br/wp-content/uploads/2020/11/rosario_dawson_mandalorian_ahsoka_character.jpg",
+  atributos: {
+    força: 8,
+    velocidade: 8,
+    habilidade: 8,
+    inteligencia: 8
+  }
+};
+var carta13 = {
+  nome: "Mandalorian",
+  avatar:
+    "https://i0.wp.com/popoca.com.br/wp-content/uploads/2020/11/The-Mandalorian.jpg",
+  atributos: {
+    força: 7,
+    velocidade: 7,
+    habilidade: 8,
+    inteligencia: 8
+  }
+};
+var carta14 = {
+  nome: "Moff Gideon",
+  avatar:
+    "https://static.wikia.nocookie.net/star-wars-canon/images/f/f5/MoffGideon.jpg",
+  atributos: {
+    força: 6,
+    velocidade: 6,
+    habilidade: 6,
+    inteligencia: 8
+  }
+};
+var carta15 = {
+  nome: "Chewbacca",
+  avatar: "https://www.hypeness.com.br/1/2019/02/chewbacca-vertical.jpg",
+  atributos: {
+    força: 6,
+    velocidade: 6,
+    habilidade: 6,
+    inteligencia: 4
+  }
+};
+var carta16 = {
+  nome: "Conde Dooku",
+  avatar:
+    "https://nathanxwinghome.files.wordpress.com/2019/03/count_dooku_4f552149.0-e1551589707307.jpeg",
+  atributos: {
+    força: 9,
+    velocidade: 5,
+    habilidade: 9,
+    inteligencia: 8
+  }
+};
+var carta17 = {
+  nome: "Han Solo",
+  avatar:
+    "https://s2.glbimg.com/adFNCY28mMYhn6cOebEGMlkE3J0=/620x520/smart/e.glbimg.com/og/ed/f/original/2013/11/09/episode_4_han_solo_2.jpg",
+  atributos: {
+    força: 5,
+    velocidade: 6,
+    habilidade: 7,
+    inteligencia: 6
+  }
+};
+var carta18 = {
+  nome: "Princess Lea",
+  avatar:
+    "https://media.glamour.com/photos/5695e9d816d0dc3747eea3f5/master/pass/beauty-2015-12-princess-leia-2-w724-main.jpg",
+  atributos: {
+    força: 5,
+    velocidade: 5,
+    habilidade: 6,
+    inteligencia: 8
+  }
+};
+var carta19 = {
+  nome: "Lando Calrissian",
+  avatar:
+    "https://static.wikia.nocookie.net/star-wars-canon-extended/images/0/0a/Lando5.jpg",
+  atributos: {
+    força: 5,
+    velocidade: 5,
+    habilidade: 5,
+    inteligencia: 6
+  }
+};
+var carta20 = {
+  nome: "Finn",
+  avatar:
+    "https://i.pinimg.com/originals/c5/e0/55/c5e05506bcbb59dcd0443ba0c5b0dd07.jpg",
+  atributos: {
+    força: 5,
+    velocidade: 7,
+    habilidade: 7,
+    inteligencia: 6
+  }
+};
+var carta21 = {
+  nome: "Poe Dameron",
+  avatar:
+    "https://static.wikia.nocookie.net/disney/images/a/ab/TLJ_-_Poe_Dameron.jpg",
+  atributos: {
+    força: 5,
+    velocidade: 6,
+    habilidade: 7,
+    inteligencia: 5
+  }
+};
+var carta22 = {
+  nome: "Stormtrooper",
+  avatar:
+    "https://static.wikia.nocookie.net/starwars/images/8/84/Unidentified_First_Order_stormtrooper.jpg",
+  atributos: {
+    força: 4,
+    velocidade: 3,
+    habilidade: 4,
+    inteligencia: 4
+  }
+};
+var carta23 = {
+  nome: "Darktrooper",
+  avatar:
+    "https://www.comingsoon.net/assets/uploads/2021/01/Hot-Toys-Mando-Dark-Trooper-Collectible-Figure_PR1.jpg",
+  atributos: {
+    força: 7,
+    velocidade: 5,
+    habilidade: 6,
+    inteligencia: 4
+  }
+};
+var carta24 = {
+  nome: "Captain Phasma",
+  avatar:
+    "https://cdn.shopify.com/s/files/1/1047/6902/products/rodel-gonzalez-star-wars-captain-phasma_1024x1024.jpg",
+  atributos: {
+    força: 5,
+    velocidade: 5,
+    habilidade: 6,
+    inteligencia: 5
+  }
+};
+var carta25 = {
+  nome: "Boba Fett",
+  avatar:
+    "https://disneyplusbrasil.com.br/wp-content/uploads/2021/10/Boba-Fett.jpg",
+  atributos: {
+    força: 5,
+    velocidade: 6,
+    habilidade: 7,
+    inteligencia: 6
+  }
+};
+
+var carta26 = {
+  nome: "Fennec Shand",
+  avatar:
+    "https://static.wikia.nocookie.net/themandalorian/images/9/9f/TBBF_Fennec_Shand.png",
+  atributos: {
+    força: 6,
+    velocidade: 8,
+    habilidade: 8,
+    inteligencia: 7
+  }
+};
+var carta27 = {
+  nome: "R2 D2",
+  avatar:
+    "https://static.turbosquid.com/Preview/2020/01/23__08_20_45/r2d2_bg.png657C0C48-1127-435A-BB78-778D471B86DFLarge.jpg",
+  atributos: {
+    força: 1,
+    velocidade: 3,
+    habilidade: 4,
+    inteligencia: 7
+  }
+};
+var carta28 = {
+  nome: "C3 PO",
+  avatar:
+    "https://www.tenhomaisdiscosqueamigos.com/wp-content/uploads/2017/01/C3PO.jpg",
+  atributos: {
+    força: 1,
+    velocidade: 2,
+    habilidade: 3,
+    inteligencia: 5
+  }
+};
+
+var carta29 = {
+  nome: "BB8",
+  avatar:
+    "https://media.contentapi.ea.com/content/dam/star-wars-battlefront-2/images/2020/01/swbf2-hero-lg-hero-bb-8-16x9-xl.jpg.adapt.crop1x1.320w.jpg",
+  atributos: {
+    força: 1,
+    velocidade: 7,
+    habilidade: 4,
+    inteligencia: 7
+  }
+};
+var carta30 = {
+  nome: "Snoke",
+  avatar:
+    "https://hips.hearstapps.com/digitalspyuk.cdnds.net/18/03/1516283720-snoke.jpg",
+  atributos: {
+    força: 8,
+    velocidade: 2,
+    habilidade: 8,
+    inteligencia: 8
+  }
+};
+
+var carta31 = {
+  nome: "Princess Amidala",
+  avatar:
+    "https://static.wikia.nocookie.net/disney/images/9/97/Padme_Amidala_AOTC.jpg",
+  atributos: {
+    força: 5,
+    velocidade: 6,
+    habilidade: 6,
+    inteligencia: 9
+  }
+};
+var carta32 = {
+  nome: "Jabba the Hutt",
+  avatar: "http://www.sw-omens.org/images/b/be/Jabba.jpg",
+  atributos: {
+    força: 2,
+    velocidade: 1,
+    habilidade: 1,
+    inteligencia: 8
+  }
+};
+
+var baralho = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22,
+  carta23,
+  carta24,
+  carta25,
+  carta26,
+  carta27,
+  carta28,
+  carta29,
+  carta30,
+  carta31,
+  carta32
+];
+var baralhoJogador = [];
+var baralhoMaquina = [];
+var cartaMaquina;
+var cartaJogador;
+var ganhador;
+var partidas = 0;
+var placar = [0, 0, 0];
+
+//-------------------------------------------------------------------------
+// função que inicia o primeiro duelo do torneio
+function iniciarJogo() {
+  resetaTela();
+
+  divideCartas();
+  sorteiaCartas();
+
+  exibeCarta(baralhoJogador[cartaJogador], "left");
+  exibeCarta(carta0, "right");
+
+  // Escolhendo o atributo de batalha
+  document.getElementById("info-center").innerHTML =
+    "Escolha o Atributo<br><br> do Combate";
+
+  // muda o contexto do botão para a opção duelar
+  var btnJogar = document.getElementById("btnJogar");
+  btnJogar.disabled = true;
+  btnJogar.classList.remove("button:active");
+  document.getElementById("btnJogar").innerHTML = "Duelar";
+}
+
+//-------------------------------------------------------------------------
+// funcao que inicia duelos seguintes do torneio
+function continuarDuelando() {
+  resetaTela();
+  sorteiaCartas();
+
+  exibeCarta(baralhoJogador[cartaJogador], "left");
+  exibeCarta(carta0, "right");
+
+  // Escolhendo o atributo de batalha
+  document.getElementById("info-center").innerHTML =
+    "Escolha o Atributo<br><br> do Combate";
+
+  // muda o contexto do botão para a opção duelar
+  document.getElementById("btnJogar").disabled = true;
+
+  document.getElementById("btnJogar").innerHTML = "Duelar";
+}
+
+//-------------------------------------------------------------------------
+// funcão que cria os baralhos do jogador e da máquina
+function divideCartas() {
+  var baralhoTemp = baralho.slice();
+  var carta;
+
+  // Limpa os baralhos
+  baralhoJogador = [];
+  baralhoMaquina = [];
+
+  while (baralhoTemp.length > 0) {
+    // Adiciona uma carta para o Jogador
+    carta = parseInt(Math.random() * baralhoTemp.length);
+    baralhoJogador.push(baralhoTemp[carta]);
+    baralhoTemp.splice(carta, 1);
+
+    // Adiciona uma carta para a maquina
+    carta = parseInt(Math.random() * baralhoTemp.length);
+    baralhoMaquina.push(baralhoTemp[carta]);
+    baralhoTemp.splice(carta, 1);
+  }
+}
+
+//-------------------------------------------------------------------------
+// função que sorteia as cartas
+function sorteiaCartas() {
+  cartaJogador = parseInt(Math.random() * baralhoJogador.length);
+  cartaMaquina = parseInt(Math.random() * baralhoMaquina.length);
+}
+
+//-------------------------------------------------------------------------
+// funcao que volta a tela para a condição de inicio do jogo
+function resetaTela() {
+  // resetanto o painel da esquerda
+  document.getElementById("left-label").style.color = "rgba(0, 0, 0, 0)";
+  document.getElementById("card-left").style.backgroundImage = "";
+  document.getElementById("attribs-left").innerHTML = "";
+
+  // resetanto o painel central
+  document.getElementById("info-center").innerHTML =
+    "Vamos Duelar! <br><br> Aceita o Desafio?";
+
+  // resetanto o painel da dieita
+  document.getElementById("right-label").style.color = "rgba(0, 0, 0, 0)";
+  document.getElementById("card-right").style.backgroundImage = "";
+  document.getElementById("attribs-right").innerHTML = "";
+}
+
+//-------------------------------------------------------------------------
+// funcao que exibe uma carta na tela
+function exibeCarta(carta, posicao) {
+  //var card = carta0;
+  var label = document.getElementById(posicao + "-label");
+  var divCard = document.getElementById("card-" + posicao);
+  var divAttribs = document.getElementById("attribs-" + posicao);
+  var attribs = "";
+
+  if (posicao == "left") {
+    // mostra os atributos da carta
+    attribs = '<br><div class="line-info">' + carta.nome + "</div><br>";
+    for (var atributo in carta.atributos) {
+      attribs +=
+        '<div class="line-info"><input class="kunai" type="radio" name="atributo"value="' +
+        atributo +
+        '" onchange="testaRadio()">';
+      attribs += atributo + ": " + carta.atributos[atributo] + "</div>";
+    }
+    divAttribs.innerHTML = attribs + "<br><br>";
+  }
+
+  // deixa o label principal do div visivel
+  label.style.color = "#dcdcdc";
+
+  // mostra a carta
+  divCard.style.backgroundImage = 'url("' + carta.avatar + '")';
+
+  //exibe o painel final de informações
+  document.getElementById("div-end").style.opacity = "0.8";
+}
+
+//-------------------------------------------------------------------------
+// funcão que controla os casos de radio não selecionados
+function testaRadio() {
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var radio in radioAtributo) {
+    if (radioAtributo[radio].checked) {
+      document.getElementById("btnJogar").disabled = false;
+      document.getElementById("btnJogar").onclick = function () {
+        jogar();
+      };
+    }
+  }
+}
+
+//-------------------------------------------------------------------------
+// funcão que verifica qual atributo foi escolhido para o duelo
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var radio in radioAtributo) {
+    if (radioAtributo[radio].checked) {
+      return radioAtributo[radio].value;
+    }
+  }
+}
+
+//-------------------------------------------------------------------------
+// funcão que dispara o duelo e compara as cartas
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var resultado = document.getElementById("info-center");
+  var radios = document.getElementsByName("atributo");
+
+  if (
+    baralhoJogador[cartaJogador].atributos[atributoSelecionado] >
+    baralhoMaquina[cartaMaquina].atributos[atributoSelecionado]
+  ) {
+    computaResultado(1);
+  } else if (
+    baralhoJogador[cartaJogador].atributos[atributoSelecionado] <
+    baralhoMaquina[cartaMaquina].atributos[atributoSelecionado]
+  ) {
+    computaResultado(2);
+  } else {
+    computaResultado(0);
+  }
+
+  if (baralhoJogador.length == 0 || baralhoMaquina.length == 0) {
+    fimDeJogo();
+  } else {
+    document.getElementById("btnJogar").onclick = function () {
+      continuarDuelando();
+    };
+  }
+}
+
+//-------------------------------------------------------------------------
+// funcão que informa os resultados de duelas e computa os pontos
+function computaResultado(resultado) {
+  var painelInfo = document.getElementById("info-center");
+  ganhador = resultado;
+  placar[resultado] += 1;
+  partidas++;
+
+  exibeCarta(baralhoMaquina[cartaMaquina], "right");
+  imprimeResultado();
+
+  // transfere para o jogador a carta da máquina
+  if (ganhador == 1) {
+    baralhoJogador.push(baralhoMaquina[cartaMaquina]);
+    baralhoMaquina.splice(cartaMaquina, 1);
+  }
+
+  // transfere para a máquina a carta do jogador
+  if (ganhador == 2) {
+    baralhoMaquina.push(baralhoJogador[cartaJogador]);
+    baralhoJogador.splice(cartaJogador, 1);
+  }
+  atualizaPlacar();
+}
+
+//-------------------------------------------------------------------------
+// funcão que imprime o resultado de um duelo
+function imprimeResultado() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divDest;
+  var text;
+  var carta;
+  var id = "empatou";
+  var idMaquina;
+
+  if (ganhador == 1) {
+    id = "ganhou";
+    idMaquina = "perdeu";
+    mensagemResultado = "Você Ganhou!";
+  } else if (ganhador == 2) {
+    id = "perdeu";
+    idMaquina = "ganhou";
+    mensagemResultado = "Você Perdeu!";
+  }
+
+  // imprime os blocos de resultado sobre as cartas
+  for (var local of ["attribs-left", "attribs-right"]) {
+    divDest = document.getElementById(local);
+    if (local == "attribs-left") {
+      carta = baralhoJogador[cartaJogador];
+    } else {
+      carta = baralhoMaquina[cartaMaquina];
+      if (ganhador != 0) {
+        id = idMaquina;
+      }
+    }
+
+    text = '<br><div class="line-info">' + carta.nome + "</div><br>";
+    var inicio = '<div class="line-info" ';
+    for (var linha in carta.atributos) {
+      text += inicio;
+      if (linha == atributoSelecionado) {
+        text += 'id="' + id + '" ';
+      }
+      text += ">" + linha + ": " + carta.atributos[linha] + "</div>";
+    }
+    divDest.innerHTML = text + "<br>";
+  }
+}
+
+//-------------------------------------------------------------------------
+function atualizaPlacar() {
+  var painelInfo = document.getElementById("info-center");
+  var labelCentral = document.getElementById("center-label");
+  var labelPartidas = document.getElementById("partidas");
+  var cartasJogador = document.getElementById("cartas-jogador");
+  var cartasMaquina = document.getElementById("cartas-maquina");
+  var mensagemResultado = "Deu Empate!";
+  var id = "empatou";
+
+  if (ganhador == 1) {
+    id = "ganhou";
+    mensagemResultado = "Você Ganhou!";
+  } else if (ganhador == 2) {
+    id = "perdeu";
+    mensagemResultado = "Você Perdeu!";
+  }
+
+  // imprime as informações do painel central
+  painelInfo.innerHTML = mensagemResultado;
+
+  //exibe placar
+  labelCentral.style.color = "#dcdcdc";
+  labelCentral.innerHTML = placar[1] + " x " + placar[2];
+
+  // exibe contagem de cartas e partida
+  labelPartidas.innerHTML = "Partidas: " + partidas;
+  cartasJogador.innerHTML = "Cartas: " + baralhoJogador.length;
+  cartasMaquina.innerHTML = "Cartas: " + baralhoMaquina.length;
+}
+
+//-------------------------------------------------------------------------
+// exibe mensagens quando um jogador fica com todas as cartas
+function fimDeJogo() {
+  var painelInfo = document.getElementById("info-center");
+  var botao = document.getElementById("btnJogar");
+  var mensagem;
+
+  if (ganhador == 1) {
+    mensagem = "Parabéns!!!<br><br>Você venceu<br><br>o Torneio!";
+  } else {
+    mensagem = "Que Pena!<br><br>Você perdeu<br><br>o Torneio!";
+  }
+
+  painelInfo.innerHTML = mensagem;
+  botao.innerHTML = "Reiniciar Jogo";
+  botao.setAttribute("onclick", "reiniciarJogo()");
+}
+
+//-------------------------------------------------------------------------
+// volta tudo ao ponto zero
+function reiniciarJogo() {
+  var painelInfo = document.getElementById("info-center");
+  var botao = document.getElementById("btnJogar");
+
+  partidas = 0;
+  placar = [0, 0, 0];
+
+  resetaTela();
+  document.getElementById("center-label").style.color = "rgba(0, 0, 0, 0)";
+  document.getElementById("div-end").style.opacity = "0";
+  document.getElementById("partidas").innerHTML = "Partidas: 0";
+  document.getElementById("cartas-jogador").innerHTML = "Cartas: 16";
+  document.getElementById("cartas-maquina").innerHTML = "Cartas: 16";
+
+  painelInfo.innerHTML = "Vamos Duelar! <br><br> Aceita o Desafio?";
+
+  botao.innerHTML = "Iniciar Torneio";
+  botao.onclick = function () {
+    iniciarJogo();
+  };
+}
